f3fe8a3607ea27d8b9604467ef29907cde17d5e5
Henry <henrykaua21@gmail.com>
2024-11-29 21:48:18 -0300

    criando tabelas de categorias

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
     const database = require('./db');
     const Produtos = require('./produtos');
     const tabelaUsuarios = require('./tabelaUsuarios');
+    const tabelaCategorias = require('./tabelaCategorias');
     await database.sync();
 })();
